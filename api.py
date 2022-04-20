3283034666d43d392c6f690efe5e588b7a99171e
Daniel Dugovic <ddugovic@users.noreply.github.com>
2022-04-20 05:36:14 -0500

    Import https://www.kaggle.com/code/mrisdal/fetch-scrabble-data-from-woogles-io/notebook

---
+++ api.py
@@ -0,0 +1,53 @@
+# Meg Risdal 2022-04-13
+# https://www.kaggle.com/code/mrisdal/fetch-scrabble-data-from-woogles-io/notebook
+import inflect
+import requests
+import json
+
+HEADERS = {
+    'authority': 'woogles.io',
+    'pragma': 'no-cache',
+    'cache-control': 'no-cache',
+    'accept': 'application/json, text/plain, */*',
+    'content-type': 'application/json;charset=UTF-8',
+    'origin': 'https://woogles.io',
+    'accept-language': 'en-US,en;q=0.9'
+}
+
+GCG_URL = 'https://woogles.io/twirp/game_service.GameMetadataService/GetGCG'
+RECENT_URL = 'https://woogles.io/twirp/game_service.GameMetadataService/GetRecentGames'
+
+def fetch_gcg(game_id):
+    params = '{"gameId":"%s"}'
+
+    print("Downloading game %s..." % game_id)
+    game_response = requests.post(GCG_URL, headers = HEADERS, data = (params % game_id))
+
+    return json.loads(game_response.text)
+
+def fetch_recent(username, num):
+    params = '{"username":"%s","numGames":%d,"offset":%d}'
+
+    result = list()
+
+    engine = inflect.engine()
+
+    print("Downloading %d %s..." % (num, engine.plural('game', num)))
+    step = 20
+    for offset in range(0, num, step):
+        size = min(step, num - offset)
+        games_response = requests.post(RECENT_URL, headers = HEADERS, data = (params % (username, size, offset)))
+
+        games = json.loads(games_response.text)
+
+        if game_count := len(games['game_info']):
+            result.extend(games['game_info'])
+        else:
+            break
+
+    with open('woogles.json', 'w') as output_file:
+        json.dump(result, output_file)
+        count = offset + game_count
+        print("Finished downloading game metadata (%d %s)." % (count, engine.plural('game', count)))
+
+    return result
---
+++ fetch.py
@@ -0,0 +1,61 @@
+# Meg Risdal 2022-04-20
+# https://www.kaggle.com/code/mrisdal/fetch-scrabble-data-from-woogles-io/notebook
+from api import *
+
+# get play data from a game id
+
+game_ids = []
+
+for game in fetch_recent('HastyBot', 1):
+    game_ids.append(game['game_id'])
+    
+with open("turns.csv", "w+", newline = "") as t:
+
+    t.write("game_id,turn_number,nickname,rack,location,move,points,score,turn_type\n")
+
+    for game_id in game_ids:
+        
+        plays = fetch_gcg(game_id)
+
+        gcg = plays['gcg']
+
+        # print(gcg)
+
+        # print(game_id)
+        
+        turn_number = 1
+        
+        for line in gcg.splitlines():
+            if not line.startswith("#"):
+                turn = line.strip(">").split(" ")
+
+                if turn[2] == "-":
+                    turn.insert(2, "")
+                    turn.insert(6, "Pass")
+                elif turn[2].startswith("-"):
+                    turn.insert(2, "")
+                    turn.insert(6, "Exchange")
+                elif turn[2] == "(time)":
+                    turn.insert(2, "")
+                    turn.insert(6, "Timeout")
+                elif turn[2].startswith("("):
+                    turn.insert(2, "")
+                    turn.insert(6, "Six-Zero Rule")
+                elif turn[1].startswith("("):
+                    turn.insert(1, "")
+                    turn.insert(2, "")
+                    turn.insert(6, "End")
+                else:
+                    turn.insert(6, "Play")
+
+                turn.insert(0, game_id)
+                turn.insert(1, str(turn_number))
+                turn[2] = turn[2].strip(":")
+
+                if(len(turn) != 9):
+                    print(turn)
+                    raise ValueError("Incorrect number of turn columns.")
+                
+                t.write(",".join(turn) + "\n")
+                
+                turn_number += 1
